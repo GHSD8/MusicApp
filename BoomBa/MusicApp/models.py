d4cbc34d33a0289fb0c2e46846727bc16a7e2271
GHSD8 <paul.otsula@strathmore.edu>
2024-02-18 16:46:55 +0300

    Commit

---
+++ BoomBa/MusicApp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Song(models.Model):
+    title = models.TextField()
+    artist = models.TextField()
+    image = models.ImageField(upload_to='songs_image/')
+    audio_file = models.FileField(upload_to='songs/')
+    album = models.ForeignKey('Album',on_delete=models.SET_NULL, null=True,blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+    class META:
+        ordering = ["title"]
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
---
+++ BoomBa/MusicApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect,render
+from .models import Song
+
+def index(request):
+    song = Song.objects.all().order_by('title')
+    song_list = list(Song.objects.all().order_by('title').values())
+    context = {
+
+        "songs": song,
+        "songs_list": song_list,
+
+    }
+    return render(request ,'base.html' ,context)
